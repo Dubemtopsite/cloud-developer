33caa5ea73df07fc4a6252284c08700a387b1ebd
Owoh Chidubem <chidubempaul91@gmail.com>
2022-10-23 07:26:09 +0100

    Upload image now working

--- course-04/project/c4-final-project-starter-code/backend/src/helpers/attachmentUtils.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/helpers/attachmentUtils.ts
@@ -1,17 +1,24 @@
-import * as AWS from 'aws-sdk'
-import * as AWSXRay from 'aws-xray-sdk'
+var AWS = require('aws-sdk');
+//const AWSXRay = require('aws-xray-sdk')
 
-const XAWS = AWSXRay.captureAWS(AWS)
-const s3 = new XAWS.S3({
+//const XAWS = AWSXRay.captureAWS(AWS)
+//const urlExpiration = process.env.SIGNED_URL_EXPIRATION
+//const urlExpiration = process.env.SIGNED_URL_EXPIRATION
+var credentials = {
+  accessKeyId: process.env.S3_ACCESS_KEY,
+  secretAccessKey : process.env.S3_SECRET_KEY
+};
+AWS.config.update({credentials: credentials, region: 'us-east-1'});
+const s3 = new AWS.S3({
     signatureVersion: 'v4'
 })
 const bucket_name = process.env.ATTACHMENT_S3_BUCKET
-const urlExpiration = process.env.SIGNED_URL_EXPIRATION
+//const urlExpiration = process.env.SIGNED_URL_EXPIRATION
 // TODO: Implement the fileStogare logic
 export async function getUploadSignedUrl(imageId: string) {
     return s3.getSignedUrl('putObject', {
       Bucket: bucket_name,
       Key: imageId,
-      Expires: urlExpiration
+      Expires: 300
     })
   }
