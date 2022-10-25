8eb77fb1cbee0367427249281e1e6c257e230709
Owoh Chidubem <chidubempaul91@gmail.com>
2022-10-25 18:13:17 +0100

    Issue solved

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
@@ -8,15 +8,6 @@ import { buildTodoItem } from '../../businessLogic/todos'
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const newTodo: CreateTodoRequest = JSON.parse(event.body)
-    //Check if the body has an emtpy value
-    if(newTodo.name.length === 0 || newTodo.dueDate.length === 0){
-      return {
-        statusCode: 400,
-        body: JSON.stringify({
-          message: "Incomplete Request Body"
-        })
-      }
-    }
     const createTodoItem = await buildTodoItem(newTodo, event)
     // TODO: Implement creating a new TODO item
     return {
--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
@@ -6,22 +6,12 @@ import { cors, httpErrorHandler } from 'middy/middlewares'
 
 //import { updateTodo } from '../../businessLogic/todos'
 import { updateTodoItemById } from '../../businessLogic/todos'
-import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+//import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
 //import { getUserId } from '../utils'
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const todoId = event.pathParameters.todoId
-    const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
-    if(updatedTodo.name.length === 0 || updatedTodo.dueDate.length === 0 || !updatedTodo.done){
-      return {
-        statusCode: 400,
-        body: JSON.stringify({
-          message: "Incomplete Request Body"
-        })
-      }
-    }
-    //const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
     await updateTodoItemById(todoId, event)
     // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
 
